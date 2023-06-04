eeb1646c92488f22c67a984dd416e4329e547d36
Krishna <libraryismyparadise@gmail.com>
2023-06-04 15:32:00 +0545

    Modify count characters

--- count_characters.rb
+++ count_characters.rb
@@ -1,10 +1,11 @@
 def count_characters(string)
-    character_count = Hash.new(0)
+    character_count = Hash.new()
   
     string.each_char do |char|
       next if char == ' '
   
       char = char.downcase
+      character_count[char] ||= 0
       character_count[char] += 1
     end
   
